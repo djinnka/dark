95cac95d7e247d4efac38401fcb4293d53db3b31
Evgeniy Yaremenko <e.yaremenko@bintime.com>
2024-03-04 17:45:53 +0200

    Update main.tf

--- postgres-terraform/main.tf
+++ postgres-terraform/main.tf
@@ -11,9 +11,9 @@ resource "aws_instance" "primary_1" {
 sudo yum update -y && sudo yum install git docker -y && sudo service docker start && sudo docker network create web
 sudo docker run --restart=unless-stopped --name=postgres -d -p 5432:5432 -e POSTGRES_DB=${var.workspace} -e POSTGRES_USER=${var.workspace} -e POSTGRES_PASSWORD=${var.password} -v /data:/var/lib/postgresql/data postgres
 sudo docker stop postgres
-t="$(curl ifconfig.me)"
+ip="$(curl ifconfig.me)"
 echo $t >/tmp/ip
-sudo echo "host    replication     dark  " + $t + "/32        md5" >> /data/pg_hba.conf
+sudo echo "host    replication     dark  $ip/32        md5" >> /data/pg_hba.conf
 sudo sed -i 's/^#wal_level.*/wal_level = replica/g' /data/postgresql.conf
 sudo sed -i 's/^#max_wal_senders.*/max_wal_senders = 10/g' /data/postgresql.conf
 sudo sed -i 's/^#wal_keep_segments.*/wal_keep_segments = 64/g' /data/postgresql.conf
